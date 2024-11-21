f5ac9dee835090b81f288c2679c35e128e298e6f
AbdulRehman0094 <abdlrhmnashfaq@gmail.com>
2024-11-22 02:07:05 +0500

    changes

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import HeroSection from './components/HeroSection'
-import GithubProjects from './components/GithubProjects'
-import SkillsSection from './components/Skills'
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import GithubProjects from "./components/GithubProjects";
+import SkillsSection from "./components/Skills";
+import WorkExperince from "./components/WorkExperience";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <WorkExperince />
       <GithubProjects />
       <SkillsSection />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
